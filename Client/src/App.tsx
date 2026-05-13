5a0a3ab1bf212ed77f73ed9a6e2fe4f89a617b75
Saloni <salonikumari81517@gmail.com>
2026-05-13 11:47:15 +0530

    Added expense tracker with add functionality

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 function App() {
   const [expense, setExpense] = 
 useState("");
+  const [amount, setAmount] = useState("");
+  const [expenses, setExpenses] = 
+useState<any[]>([]);
+//function to add expense
+  const addExpense = () => {
+    if (!expense || !amount) return;
+    const newExpense = {
+      name: expense,
+      amount: Number(amount)
+    };
+    setExpenses([...expenses,
+newExpenses]);
+    setExpense("");
+    setAmount("");
+  };
+
   return (
     <div>
       <h1>Finance Tracker</h1>
@@ -11,10 +27,26 @@ useState("");
        onChange={(e) => 
 setExpense(e.target.value)} 
       />
+      <input
+       type="number"
+       placeholder="Enter amount"
+       value={amount}
+       onChange={(e) =>
+      setAmount(e.target.value)}
+        type="number"
+      />
       <button onClick={() => 
 alert(expense)}>
        Add Expense 
       </button>
+
+      <ul>
+        {expenses.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.amount}
+          </li>
+        ))}
+      </ul>   
     </div>
   );
 }
